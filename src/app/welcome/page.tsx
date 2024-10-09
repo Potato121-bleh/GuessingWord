7df62b17c0e7861b2886ac5c4188980078c1ee41
vynn122 <hemtheavin@gmail.com>
2024-10-09 08:38:09 +0700

    pull

---
+++ src/app/welcome/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+
+const page = () => {
+  return (
+    <div>
+      <div className="cloud-con">
+        <Image src="" width={100} height={100} alt="cloud" />
+      </div>
+    </div>
+  );
+};
+
+export default page;
