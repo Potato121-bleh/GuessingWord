35501d843379b0fbd48d31bc0dea79e7709c9ed7
Ly Sochetra <lysochetra95@gmail.com>
2024-10-09 10:26:41 +0700

    adding cloud

--- src/app/guess-the-word/page.tsx
+++ src/app/guess-the-word/page.tsx
@@ -1,8 +1,46 @@
 import WordBox from '../component/wordBox/wordBox'
+import Image from 'next/image'
 import './guessTheWord.css'
 
 export default function GuessPage() {
     return (
+        <ul className="main-con-page">
+            <li className="title-page">
+                <h1>GUESS THE WORD</h1>
+                <ul className="cloud-con-abs">
+                    <li className="cloud-element">
+                        <Image
+                            className="cloud-img-one"
+                            src="/cloud.png"
+                            width={100}
+                            height={100}
+                            alt="Cloud img"
+                        />
+                    </li>
+                    <li className="cloud-element">
+                        <Image
+                            className="cloud-img-two"
+                            src="/cloud.png"
+                            width={100}
+                            height={100}
+                            alt="Cloud img"
+                        />
+                    </li>
+                    <li className="cloud-element">
+                        <Image
+                            className="cloud-img-three"
+                            src="/cloud.png"
+                            width={100}
+                            height={100}
+                            alt="Cloud img"
+                        />
+                    </li>
+                </ul>
+            </li>
+            <ul className="content-page"></ul>
+        </ul>
+
+        /*
         <ul className="man">
             <li className="head-man"></li>
             <li className="body-man"></li>
@@ -15,5 +53,6 @@ export default function GuessPage() {
                 <li className="right-hand-man right-hand"></li>
             </ul>
         </ul>
+        */
     )
 }
