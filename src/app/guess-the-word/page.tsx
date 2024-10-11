f433975a1027e2e1a8e805fca6149e016060441b
Ly Sochetra <lysochetra95@gmail.com>
2024-10-11 09:58:42 +0700

    adding update main system of game

--- src/app/guess-the-word/page.tsx
+++ src/app/guess-the-word/page.tsx
@@ -1,8 +1,181 @@
+'use client'
+
 import WordBox from '../component/wordBox/wordBox'
 import Image from 'next/image'
 import './guessTheWord.css'
+import { redirect, useRouter } from 'next/navigation'
+import { LegacyRef, useRef, useState } from 'react'
+import { nameEasyArr, type nameEasyArrType } from './words'
+
+const handleRandom = (NumOfRand: number) => {
+    let randomIntTem: number = Math.floor(Math.random() * NumOfRand)
+    return randomIntTem
+}
+let tracker: number = 0
+let selectedWordPlayV: string[] = []
+let randomInt4: number = handleRandom(5) //0 -> 4
+let randomInt3: number = handleRandom(4) //0 -> 3
+//let randomInt50: number = handleRandom(50)
+let randomInt25: number = handleRandom(25) //0 -> 24
+let heartArr: number[] = [1, 1, 1, 1, 1, 1]
+
+//HERE IS WHAT WE DONE:
+//we try already built the system to validate before open for play
+//and it will adding letter on every refresh of page. (we can try using tracker) //PAY ATTENTION TO THAT
+//and we also build the system of how it should behave
 
 export default function GuessPage() {
+    console.log('THE PAGE HAS RENDERED')
+    let [test, setest] = useState()
+    const guessRef = useRef<HTMLButtonElement>(null)
+    const inputRef = useRef<HTMLInputElement>(null)
+    const startRef = useRef<HTMLButtonElement>(null)
+    let [hint, setHint] = useState<string>('')
+    let router = useRouter()
+    tracker++
+    let [selectedWordPlayForRender, setselectedWordPlayForRender] = useState<
+        string[]
+    >([])
+    let [heartArrforRender, setheartArrforRender] = useState<number[]>([])
+
+    let [triggerRender, setTriggerRender] = useState<boolean>(false)
+
+    //let randomInt50: number = handleRandom(50)
+
+    let selectedWord = nameEasyArr[randomInt25]
+    console.log(selectedWord)
+    let selectedWordArr = selectedWord.name.split('')
+    console.log(selectedWordArr)
+    console.log(tracker)
+    //ALL CODE BELOW HERE IS FOR VALIDATION BEFORE GAME START
+
+    const handleStartGame = () => {
+        setheartArrforRender([1, 1, 1, 1, 1, 1])
+        for (let i = 0; i < selectedWordArr.length; i++) {
+            selectedWordPlayV.push('')
+        }
+        if (selectedWordArr.length == 4) {
+            console.log('it has 4 length')
+            let hintChar: string = selectedWordArr[randomInt3]
+            for (let i = 0; i < selectedWordArr.length; i++) {
+                if (selectedWordArr[i] == hintChar) {
+                    selectedWordPlayV.splice(i, 1, hintChar)
+                    console.log(selectedWordPlayV)
+                    setselectedWordPlayForRender(selectedWordPlayV)
+                    //setTriggerRender((prev) => !prev)
+                    break
+                }
+            }
+        }
+        if (selectedWordArr.length == 5) {
+            console.log('it has 5 length')
+            let hintChar: string = selectedWordArr[randomInt4]
+            console.log(hintChar)
+            for (let i = 0; i < selectedWordArr.length; i++) {
+                if (selectedWordArr[i] == hintChar) {
+                    selectedWordPlayV.splice(i, 1, hintChar)
+                    console.log(selectedWordPlayV)
+                    setselectedWordPlayForRender(selectedWordPlayV)
+                    //setTriggerRender((prev) => !prev)
+                    console.log('IT PASSED THE RENDERED')
+                }
+            }
+        }
+        if (startRef.current) {
+            startRef.current.style.display = 'none'
+        }
+        if (inputRef.current) {
+            inputRef.current.style.display = 'block'
+        }
+        if (guessRef.current) {
+            guessRef.current.style.display = 'block'
+        }
+        //setTriggerRender((prev) => !prev)
+        setselectedWordPlayForRender(selectedWordPlayV)
+    }
+
+    //CODE BELOW HERE IS WHEN USER SUBMIT DATA
+    const handleGuess = () => {
+        let inputElement = document.getElementById(
+            'input-typehere-id'
+        ) as HTMLInputElement
+        //COULD BE AN ERROR CONVERTING INTO UPPERCASE
+        let userinput = inputElement.value.toUpperCase()
+        if (userinput) {
+            let inputCharArr = userinput.split('')
+            if (inputCharArr.length > 1) {
+                window.alert('Guess one character only!!')
+                return
+            } else {
+                console.log(selectedWordPlayV)
+                let wordForChecking = [...selectedWordPlayV]
+
+                //Doing validation of how system adding userinput into wordBox
+                for (let i = 0; i < selectedWordArr.length; i++) {
+                    if (selectedWordArr[i] == userinput) {
+                        selectedWordPlayV.splice(i, 1, userinput)
+                        //setselectedWordPlayForRender(selectedWordPlayV)
+                    }
+                }
+
+                //We checking if the player guess correct or not
+                if (
+                    wordForChecking.every(
+                        (element, index) => element == selectedWordPlayV[index]
+                    )
+                ) {
+                    window.alert('YOU LOSE 1 LIFE!!')
+
+                    //We check if it only has 4 life we give a hint1
+                    if (heartArr[1] == 0 && heartArr[2] == 1) {
+                        setHint('Hint: ' + selectedWord.hint1)
+                    } else if (heartArr[3] == 0 && heartArr[4] == 1) {
+                        setHint('Hint: ' + selectedWord.hint2)
+                    }
+                    //We check if it only has 2 life we give a hint2
+                    //For deleting player heart
+                    //Check before delete it
+                    let heartArrValidation = [0, 0, 0, 0, 0, 0]
+                    if (
+                        heartArr.every(
+                            (element, index) =>
+                                element == heartArrValidation[index]
+                        )
+                    ) {
+                        console.log('PLAYER HAS NO MORE LIVE')
+                        //redirect('/tryAgain')
+                    } else {
+                        console.log('PLAYER STILL HAVE LIFE')
+                        for (let i = 0; i < heartArr.length; i++) {
+                            if (heartArr[i] == 1) {
+                                heartArr[i] = 0
+                                console.log(heartArr)
+                                let heartArrPrep = [...heartArr]
+                                setheartArrforRender(heartArrPrep)
+                                break
+                            }
+                        }
+                    }
+                } else {
+                    let selectedWordPlayVPrep = [...selectedWordPlayV]
+                    setselectedWordPlayForRender(selectedWordPlayVPrep)
+                }
+
+                //Last checking whether all box are filled or not
+                for (let i = 0; i < selectedWordPlayV.length; i++) {
+                    if (!selectedWordPlayV[i]) {
+                        return
+                    }
+                }
+                //REDIRECT IS NOT WORKING
+                window.alert('THE PLAYER HAS WIN')
+                //redirect('/Winnder-Winner-chicken-dinnner')
+            }
+        } else {
+            window.alert('Please Guess something!')
+        }
+    }
+
     return (
         <ul className="main-con-page">
             <li className="title-page">
@@ -37,7 +210,58 @@ export default function GuessPage() {
                     </li>
                 </ul>
             </li>
-            <ul className="content-page"></ul>
+            <ul className="content-page">
+                <ul className="content-leftside">
+                    <ul className="content-wordbox-con">
+                        {selectedWordPlayForRender.map((element) => {
+                            return (
+                                <li>
+                                    <WordBox letter={element} />
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <li className="content-btn-con">
+                        <input
+                            ref={inputRef}
+                            id="input-typehere-id"
+                            placeholder="Type Here"
+                            className="content-btn-typehere"
+                        />
+                        <button
+                            ref={guessRef}
+                            className="content-btn-guess"
+                            onClick={handleGuess}
+                        >
+                            Guess
+                        </button>
+                        <button
+                            ref={startRef}
+                            className="content-btn-start"
+                            onClick={handleStartGame}
+                        >
+                            Start Now
+                        </button>
+                    </li>
+                    <li>
+                        <p>{hint}</p>
+                    </li>
+                </ul>
+                <ul className="content-rightside">
+                    {heartArrforRender.map((element) => {
+                        return (
+                            <li>
+                                <Image
+                                    src={'/' + element + '.png'}
+                                    alt={''}
+                                    width={75}
+                                    height={75}
+                                />
+                            </li>
+                        )
+                    })}
+                </ul>
+            </ul>
         </ul>
 
         /*
---
+++ src/app/guess-the-word/words.ts
@@ -0,0 +1,398 @@
+
+export type nameEasyArrType = {
+  id: number,
+  name: string,
+  hint1: string,
+  hint2: string,
+  definition: string
+}
+
+
+
+export let nameEasyArr: nameEasyArrType[] = [
+  {
+    id: 1,
+    name: "APRIL",
+    hint1: "it's a month",
+    hint2: "this month is before June(Month)",
+    definition:
+      "the fourth month of the year, in the northern hemisphere usually considered the second month of spring:",
+  },
+  {
+    id: 2,
+    name: "AGE",
+    hint1: "the time of life",
+    hint2: " how old someone is.",
+    definition:
+      "the length of time that a person has lived or a thing has existed",
+  },
+  {
+    id: 3,
+    name: "AIR",
+    hint1: "oxygen",
+    hint2: "no smell, no body, can't see and touch it.",
+    definition:
+      "the invisible gaseous substance surrounding the earth, a mixture mainly of oxygen and nitrogen",
+  },
+  {
+    id: 4,
+    name: "ADD",
+    hint1: "attach",
+    hint2: "join something or make something more",
+    definition:
+      "join something to something else so as to increase the size, numberr or amount.",
+  },
+  {
+    id: 5,
+    name: "ARM",
+    hint1: "related to your body",
+    hint2: "if you want to hold something you need to use those",
+    definition:
+      "he two long parts of the upper body that are attached to the shoulders and have the hands at the end",
+  },
+  {
+    id: 6,
+    name: "ASK",
+    hint1: "same as request",
+    hint2: "what will you do when you want to know about something",
+    definition:
+      "he two long parts of the upper body that are attached to the shoulders and have the hands at the end",
+  },
+  {
+    id: 7,
+    name: "APPLE",
+    hint1: "fruit",
+    hint2: "fruit that have only red and green or yellow color",
+    definition:
+      "a round fruit with firm, white flesh and a green, red, or yellow skin:",
+  },
+  {
+    id: 8,
+    name: "BAG",
+    hint1: "use to carry somethings",
+    hint2: "same as backpack",
+    definition:
+      "a soft container made out of paper or thin plastic, and open at the top, used to hold foods and other goods",
+  },
+  {
+    id: 9,
+    name: "BAT",
+    hint1: "animal",
+    hint2: "animal that can fly and have black skin",
+    definition: "a small animal like a mouse with wings that flies at night",
+  },
+  {
+    id: 10,
+    name: "BEE",
+    hint1: "animal",
+    hint2: "animal that can fly and sting",
+    definition:
+      "a yellow and black flying insect that makes honey and can sting you",
+  },
+  {
+    id: 11,
+    name: "BOOK",
+    hint1: "have page",
+    hint2: "use to written text or noted",
+    definition:
+      "a set of pages that have been fastened together inside a cover to be read or written in",
+  },
+  {
+    id: 12,
+    name: "BIG",
+    hint1: "larg",
+    hint2: "opposite of small",
+    definition: "large in size or amount",
+  },
+  {
+    id: 13,
+    name: "COOK",
+    hint1: "to make food",
+    hint2: "when you hungry you have to?",
+    definition:
+      "prepare (food, a dish, or a meal) by mixing, combining, and heating the ingredients",
+  },
+  {
+    id: 14,
+    name: "CAN",
+    hint1: "It a verb",
+    hint2: "you be able to do sth...",
+    definition:
+      "to decide that an organized event will not happen, or to stop an order for goods or services that you no longer want",
+  },
+  {
+    id: 15,
+    name: "CAT",
+    hint1: "animal",
+    hint2: "this animal used to meow meow",
+    definition:
+      "a small animal with fur, four legs, a tail, and claws, usually kept as a pet or for catching mice",
+  },
+  {
+    id: 16,
+    name: "DADDY",
+    hint1: "human",
+    hint2: "he is man and also your parent",
+    definition: "he is your parent",
+  },
+  {
+    id: 17,
+    name: "EYE",
+    hint1: "on your face",
+    hint2: "on your face and your nose, under the eyebrow",
+    definition: "one of the two organs in your face that are used for seeing",
+  },
+  {
+    id: 18,
+    name: "FIGHT",
+    hint1: "kick or slap",
+    hint2: "hitting or kicking someone",
+    definition:
+      "to use physical force to try to defeat another person or group of people",
+  },
+  {
+    id: 19,
+    name: "FAST",
+    hint1: "quick",
+    hint2: "opposite of slow",
+    definition:
+      "to use physical force to try to defeat another person or group of people",
+  },
+  {
+    id: 20,
+    name: "GET",
+    hint1: "obtain",
+    hint2: "receive something from someone",
+    definition: "to obtain, buy, or earn something",
+  },
+  {
+    id: 21,
+    name: "HOT",
+    hint1: "you can't not use your hand to touch it",
+    hint2: "opposite of cold",
+    definition: "having a high temperature",
+  },
+  {
+    id: 22,
+    name: "HAIR",
+    hint1: "some long and some short",
+    hint2: "on your head",
+    definition:
+      "the mass of thin thread-like structures on the head of a person, or any of these structures that grow out of the skin of a person or animal",
+  },
+  {
+    id: 23,
+    name: "LOVE",
+    hint1: "obsess",
+    hint2: "when you have feeling for someone and they also have for you also",
+    definition: "an intense feeling of deep affection",
+  },
+  {
+    id: 24,
+    name: "MAN",
+    hint1: "human",
+    hint2: "opposite of woman",
+    definition: "an adult male human being",
+  },
+  {
+    id: 25,
+    name: "NEED",
+    hint1: "same as wanted",
+    hint2: "want something so much",
+    definition:
+      "require (something) because it is essential or very important rather than just desirable",
+  },
+];
+
+let nameNormalArr = [
+  {
+    id: 1,
+    name: "AIRPORT",
+    hint1: "plane lane",
+    hint2: "a station of plan",
+    definition:
+      "a place where aircraft regularly take off and land, with buildings for passengers to wait in",
+  },
+  {
+    id: 2,
+    name: "AIR CONDITIONER",
+    hint1: "machine for cooler",
+    hint2: "use it in the building or house for make them cooler",
+    definition: "a machine that keeps the air in a building cool",
+  },
+  {
+    id: 3,
+    name: "ANSWER",
+    hint1: "giving or return",
+    hint2: "reply or respone someone",
+    definition:
+      "a thing that is said, written, or done as a reaction to a question, statement, or situation",
+  },
+  {
+    id: 4,
+    name: "BOTTLE",
+    hint1: "use to keep water",
+    hint2:
+      "a glass or plastic container with a narrow neck, used for storing drinks or other liquids",
+    definition:
+      "a container for liquids, usually made of glass or plastic, with a narrow neck",
+  },
+  {
+    id: 5,
+    name: "BRIDGE",
+    hint1: "use to across over the water",
+    hint2: " a structure built to across one place to one place",
+    definition:
+      "a structure that is built over a river, road, or railway to allow people and vehicles to cross from one side to the other",
+  },
+  {
+    id: 6,
+    name: "BUTTERFLY",
+    hint1: "animal",
+    hint2: "have a small body and wing love flower ",
+    definition: "a type of insect with large, often brightly coloured wings",
+  },
+  {
+    id: 7,
+    name: "CANDLE",
+    hint1: "can make a light",
+    hint2: "it's same as lantern or lighthouse",
+    definition:
+      "a stick-shaped piece of wax with a wick (= piece of string) in the middle of it that produces light as it slowly burns",
+  },
+  {
+    id: 8,
+    name: "CREATE",
+    hint1: "making",
+    hint2: "building or making something",
+    definition: "to make something new, or invent something",
+  },
+  {
+    id: 9,
+    name: "CHANCE",
+    hint1: "access to do",
+    hint2: "have opportunity",
+    definition: "an occasion that allows something to be done",
+  },
+  {
+    id: 10,
+    name: "DANGER",
+    hint1: "warning",
+    hint2: "get into harm or death",
+    definition: "the possibility of harm or death to someone",
+  },
+  {
+    id: 11,
+    name: "DIVIDE",
+    hint1: "break down something",
+    hint2: "cut out something into two things up",
+    definition: "to (cause to) separate into parts or groups",
+  },
+  {
+    id: 12,
+    name: "ELEPHANt",
+    hint1: "animal",
+    hint2: "have a big body and have four legs with long nose",
+    definition:
+      "a very large grey mammal that has a trunk (= long nose) with which it can pick things up",
+  },
+  {
+    id: 13,
+    name: "ENOUGH",
+    hint1: "don't need something more",
+    hint2: "as much as required",
+    definition:
+      "as much as is necessary; in the amount or to the degree needed",
+  },
+  {
+    id: 14,
+    name: "FISHERMAN",
+    hint1: "people catch animal",
+    hint2: "a person who catching fish",
+    definition: "someone who catches fish, especially as a job",
+  },
+  {
+    id: 15,
+    name: "FOLLOW",
+    hint1: "do the same thing",
+    hint2: "to get after or come behind",
+    definition:
+      "to move behind someone or something and go where he, she, or it goes",
+  },
+  {
+    id: 16,
+    name: "GANGSTER",
+    hint1: "bad people",
+    hint2: "a group of bad peple on street",
+    definition: "a member of an organized group of violent criminals",
+  },
+  {
+    id: 17,
+    name: "GEOGRAPHY",
+    hint1: "a subject in school",
+    hint2: "a subject that study about peple community",
+    definition:
+      "the study of the systems and processes involved in the world's weather, mountains, seas, lakes, etc. and of the ways in which countries and people organize life within an area",
+  },
+  {
+    id: 18,
+    name: "LIGHTNING",
+    hint1: "will appear when rain",
+    hint2: "same as thunder but have more bright ligh",
+    definition:
+      "a flash of bright light in the sky that is produced by electricity moving between clouds or from clouds to the ground",
+  },
+  {
+    id: 19,
+    name: "LIPSTICKS",
+    hint1: "use on mouth",
+    hint2: "many girl use it on their mouth",
+    definition:
+      "a coloured substance that is put on the lips to improve or change their colour",
+  },
+  {
+    id: 20,
+    name: "MONDAY",
+    hint1: "weekday",
+    hint2: "a day before Tuesday",
+    definition: "the day of the week after Sunday and before Tuesday",
+  },
+  {
+    id: 21,
+    name: "OBSESS",
+    hint1: "interest",
+    hint2: "inlove or interest into something",
+    definition:
+      "If something or someone obsesses you, or if you obsess about something or someone, you think about it, him, or her all the time",
+  },
+  {
+    id: 22,
+    name: "NICE",
+    hint1: "Be good",
+    hint2: "doing good or be Nice...",
+    definition: "Doing something good to other",
+  },
+  {
+    id: 23,
+    name: "GAME",
+    hint1: "it's sth boy love to play",
+    hint2: "It can play on TV or console",
+    definition:
+      "a sweet, usually brown, food made from cacao seeds, that is usually sold in a block and is often used as a flavour for desserts, drinks",
+  },
+  {
+    id: 24,
+    name: "REPORT",
+    hint1: "telling something after knowing from someone",
+    hint2: "a formal account of something",
+    definition:
+      "to give a description of something or information about it to someone",
+  },
+  {
+    id: 25,
+    name: "SILENT",
+    hint1: "no sound",
+    hint2: "no sound and quiet",
+    definition: "without sound or noise",
+  },
+];
